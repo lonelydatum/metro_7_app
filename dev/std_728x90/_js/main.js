896d706f42fafd8f1ea7c93b71a4aa4857bac25e
Gar <gar@lonelydatu.com>
2020-09-03 19:41:07 -0400

    On

---
+++ dev/std_728x90/_js/main.js
@@ -0,0 +1,16 @@
+function start(){
+	TweenLite.defaultEase = Power3.easeInOut
+	const tl = new TimelineMax()
+	tl.set(".frame1", {opacity:1})
+	// tl.from(".logo_metro", .3, {opacity:0}, "+=.3")
+	tl.from(".phone", .5, {y:"+=90"}, "+=.3")
+	tl.from(".txt", .3, {opacity:0}, "+=.3")
+	
+	tl.from([".logo", ".app"], .5, {opacity:0}, "+=.5")
+}
+
+start()
+
+
+module.exports = {};
+
