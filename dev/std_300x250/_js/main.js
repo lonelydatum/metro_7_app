df909278745f3502fe2ef3efa5aae1f202ab202f
Gar <gar@lonelydatu.com>
2020-09-03 22:04:42 -0400

    On

--- dev/std_300x250/_js/main.js
+++ dev/std_300x250/_js/main.js
@@ -2,7 +2,7 @@ function start(){
 	TweenLite.defaultEase = Power3.easeInOut
 	const tl = new TimelineMax()
 	tl.set(".frame1", {opacity:1})
-	tl.to(".childholder", .5, {x:0}, "+=.2")
+	// tl.to(".childholder", .5, {x:0}, "+=.2")
 	tl.to(".childholder", .5, {x:-148}, "+=1")	
 	tl.from(".app", .5, {opacity:0}, "+=.3")
 }
